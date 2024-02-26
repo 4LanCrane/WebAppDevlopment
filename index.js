47b21b6a124bdf953518f24d7fb95f3648f6d77c
4LanCrane <125448543+4LanCrane@users.noreply.github.com>
2024-02-26 15:28:49 +0000

    added extra checks

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ window.addEventListener("load", function () {
   form.addEventListener("submit", sendMessage);
 
   document.querySelector("#imageCheckbox").checked = true;
-  var searchboxHint = document.querySelector(".searchboxHint");
-  searchboxHint.classList.add("hide");
+  var searchError = document.querySelector(".searchError");
+  searchError.classList.add("hide");
 });
 
 async function sendMessage(evt) {
@@ -16,13 +16,16 @@ async function sendMessage(evt) {
   let loadImages = true;
   let pageSize = document.querySelector("#resultAmountDropdown").value;
   let loading = document.querySelector("#loading");
-  let searchboxHint = document.querySelector(".searchboxHint");
+  var searchError = document.querySelector(".searchError");
 
   console.log(pageSize);
 
   if (userInput === "" || userInput.length < 0) {
+    searchError.classList.remove("hide");
+    searchError.textContent = "Please enter a search term";
     fieldValid = false;
   } else {
+    searchError.classList.add("hide");
     fieldValid = true;
   }
 
@@ -32,17 +35,12 @@ async function sendMessage(evt) {
     loadImages = false;
   }
 
-  if(!fieldValid){
-    searchboxHint.textContent = "Please enter a search term";
-   searchboxHint.classList.remove("hide");
-  }
-
-
   if (fieldValid) {
     console.log(userInput);
-    searchboxHint.classList.add("hide");
     // send a request to the server to get the data using the fetch API and the user input and store the response in a variable
-    const response = await fetch(
+    var resultsContainer = document.querySelector("#results");
+
+    await fetch(
       "https://api.vam.ac.uk/v2/objects/search?q=" +
         userInput +
         "&data_profile=full" +
@@ -50,130 +48,125 @@ async function sendMessage(evt) {
         pageSize +
         "&images=" +
         loadImages
-    ).catch(function (error) {
-      console.log(
-        "There has been a problem with your fetch operation: " + error.message
-      );
-      alert(
-        "There has been a problem with your fetch operation: " + error.message);
-    });
+    )
+      .then(function (response) {
+        if (response.ok) {
+          //show the loading spinner
+          loading.classList.add("show");
+          //hide the results container
+          resultsContainer.classList.remove("show");
+          return response;
+        }
+      })
+      .then(async (response) => {
+        console.log(response);
 
+        searchError.classList.add("hide");
 
-    var resultsContainer = document.querySelector("#results");
+        // store the response in a variable
+        const data = await response.json();
 
-    //if the responce is okay do the following
-    if (response.ok) {
-      //show the loading spinner
-      loading.classList.add("show");
-      //hide the results container
-      resultsContainer.classList.remove("show");
-    }
+        // log the data to the console
+        console.log(data);
 
-    // store the response in a variable
-    const data = await response.json();
+        // create a variable to store the results
+        var results = data.records;
 
-    // log the data to the console
-    console.log(data);
+        // log the results to the console
+        console.log(results);
 
-    // create a variable to store the results
-    var results = data.records;
+        // Clear previous results
+        while (resultsContainer.firstChild) {
+          resultsContainer.removeChild(resultsContainer.firstChild);
+        }
 
-    // log the results to the console
-    console.log(results);
+        // Iterate over the results and create HTML elements for each item
+        results.forEach((result) => {
+          // Create a container for each item
+          var itemContainer = document.createElement("div");
+          itemContainer.classList.add("item");
 
-    // check if the results are empty
-    if (results.length === 0) {
-      searchboxHint.textContent = "No results found";
-      searchboxHint.classList.remove("hide");
-    }
+          // Create a heading element
+          var heading = document.createElement("h2");
+          if (result._primaryTitle === "") {
+            heading.textContent = "No title available";
+          } else {
+            heading.textContent = result._primaryTitle;
+          }
+          itemContainer.appendChild(heading);
 
-    // Clear previous results
-    while (resultsContainer.firstChild) {
-      resultsContainer.removeChild(resultsContainer.firstChild);
-    }
+          var date = document.createElement("p");
+          if (result._primaryDate === "") {
+            date.textContent = "No date available";
+          } else {
+            date.textContent = "Date:" + result._primaryDate;
+          }
+          itemContainer.appendChild(date);
 
-    // Iterate over the results and create HTML elements for each item
-    results.forEach((result) => {
-      // Create a container for each item
-      var itemContainer = document.createElement("div");
-      itemContainer.classList.add("item");
+          var descriptionTitle = document.createElement("h3");
+          descriptionTitle.textContent = "Description";
+          itemContainer.appendChild(descriptionTitle);
 
-      // Create a heading element
-      var heading = document.createElement("h2");
-      if (result._primaryTitle === "") {
-        heading.textContent = "No title available";
-      } else {
-        heading.textContent = result._primaryTitle;
-      }
-      itemContainer.appendChild(heading);
+          var description = document.createElement("p");
 
-      var date = document.createElement("p");
-      if (result._primaryDate === "") {
-        date.textContent = "No date available";
-      } else {
-        date.textContent = "Date:" + result._primaryDate;
-      }
-      itemContainer.appendChild(date);
-     
-      var descriptionTitle = document.createElement("h3");
-      descriptionTitle.textContent = "Description";
-      itemContainer.appendChild(descriptionTitle);
+          // set description to the summary description and remove any html tags
+          description.textContent = result.summaryDescription.replaceAll(
+            /<[^>]*>?/gm,
+            ""
+          );
+          if (result.summaryDescription === "") {
+            description.textContent = "No description available";
+          }
+          itemContainer.appendChild(description);
 
-      var description = document.createElement("p");
+          var image = result._images._primary_thumbnail;
+          if (image === undefined) {
+            var noImage = document.createElement("p");
+            noImage.textContent = "No image available";
+            itemContainer.appendChild(noImage);
+          } else {
+            //creates a new image element and check the src is loading or
+            var img = new Image();
+            img.src = image;
+            img.onerror = function () {
+              itemContainer.removeChild(img);
+              var noImage = document.createElement("p");
+              noImage.textContent = "(Image failed to load)";
+              itemContainer.appendChild(noImage);
+            };
 
-      // set description to the summary description and remove any html tags
-      description.textContent = result.summaryDescription.replaceAll(
-        /<[^>]*>?/gm,
-        ""
-      );
-      if (result.summaryDescription === "") {
-        description.textContent = "No description available";
-      }
-      itemContainer.appendChild(description);
+            img.onload = function () {
+              itemContainer.appendChild(img);
+            };
+          }
 
+          var fullSizedImageUrl = `${result._images._iiif_image_base_url}/full/full/0/default.jpg`;
 
-      var image = result._images._primary_thumbnail;
-      if (image === undefined) {
-        var noImage = document.createElement("p");
-        noImage.textContent = "No image available";
-        itemContainer.appendChild(noImage);
-      }else{
-       //creates a new image element and check the src is loading or
-      var img = new Image();
-      img.src = image;
-      img.onerror = function(){
-        itemContainer.removeChild(img);
-        var noImage = document.createElement("p");
-        noImage.textContent = "(Image failed to load)";
-        itemContainer.appendChild(noImage);
-      };
+          // Creating a new Image element for full-size image
+          var fullSizeImage = new Image();
+          fullSizeImage.src = fullSizedImageUrl;
+          fullSizeImage.classList.add("fullsizeImage");
 
-      img.onload = function(){
-        itemContainer.appendChild(img);
-      };}
+          // Append the full-size image to the image container
+          itemContainer.appendChild(fullSizeImage);
+          fullSizeImage.classList.add("hide");
 
-  
-      var fullSizedImageUrl = `${result._images._iiif_image_base_url}/full/full/0/default.jpg`;
+          itemContainer.addEventListener("click", function () {
+            fullSizeImage.classList.toggle("hide");
+            img.classList.toggle("hide");
+          });
 
-      // Creating a new Image element for full-size image
-      var fullSizeImage = new Image();
-      fullSizeImage.src = fullSizedImageUrl;
-      fullSizeImage.classList.add("fullsizeImage");
-  
-      // Append the full-size image to the image container
-      itemContainer.appendChild(fullSizeImage);
-      fullSizeImage.classList.add("hide");
+          // Append the item container to the results container
+          resultsContainer.appendChild(itemContainer);
+        });
 
-      itemContainer.addEventListener("click", function () {
-        fullSizeImage.classList.toggle("hide");
-        img.classList.toggle("hide");
+        loading.classList.remove("show");
+        resultsContainer.classList.add("show");
+      })
+      .catch(function (error) {
+       searchError.textContent = "An error occurred";
+        searchError.classList.remove("hide");
+          console.log(error);
       });
-
-      // Append the item container to the results container
-      resultsContainer.appendChild(itemContainer);
-    });
-
-    loading.classList.remove("show");
-    resultsContainer.classList.add("show");
   }
 }
